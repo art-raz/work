a1c4db25ff447e2a85c9e72ecbd30df34ecbea2d
art-raz <123168316+art-raz@users.noreply.github.com>
2023-04-09 17:10:44 +0300

    Update open or close port.sh

--- open or close port.sh
+++ open or close port.sh
@@ -1,1 +1,9 @@
-s
+#!/bin/bash
+
+#Проверить открыт/закрыт порт
+
+#sudo snap install nmap
+
+read -p "Введите порт: " PORT_NUMBER
+
+nmap localhost | grep "$PORT_NUMBER" | awk '{ print $2,$3 }' | column -t
